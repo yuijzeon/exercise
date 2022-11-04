06b9bd11857d17c0106a1a4cbb8f156a3b2d8b5b
Tintin <yuijuhn.ting@titansoft.com.sg>
2022-11-05 01:58:05 +0800

    rename

--- Katas/Katas.UnitTests/StringExtensionsTests.cs
+++ Katas/Katas.UnitTests/StringExtensionsTests.cs
@@ -4,30 +4,18 @@ namespace Katas.UnitTests;
 
 public static class StringExtensions
 {
-    public static string JoinCamelCase(this string value, string separator)
+    public static string JoinCamel(this string value, string separator = " ")
     {
-        return Regex.Replace(value, @"(?<=[a-z])\B(?=[A-Z])|(?<=\d)\B(?=\D)|(?<=\D)\B(?=\d)", separator);
+        return Regex.Replace(value, @"(?<=[\da-z])(?=[A-Z])|(?<=[a-z])(?=[\dA-Z])", separator);
     }
 }
 
 [TestFixture]
 public class StringExtensionsTests
 {
-    [TestCase("11", " ", ExpectedResult = "11")]
-    [TestCase("1a", " ", ExpectedResult = "1 a")]
-    [TestCase("1A", " ", ExpectedResult = "1 A")]
-    [TestCase("1-", " ", ExpectedResult = "1-")]
-    [TestCase("_A", " ", ExpectedResult = "_A")]
-    [TestCase("__", " ", ExpectedResult = "__")]
-    [TestCase("_-", " ", ExpectedResult = "_-")]
-    [TestCase("aa", " ", ExpectedResult = "aa")]
-    [TestCase("Aa", " ", ExpectedResult = "Aa")]
-    [TestCase("AA", " ", ExpectedResult = "AA")]
-    [TestCase("aA", " ", ExpectedResult = "a A")]
-    [TestCase("a1", " ", ExpectedResult = "a 1")]
-    [TestCase("KCPay", " ", ExpectedResult = "KCPay")]
-    public string JoinCamelCase(string value, string separator)
+    [TestCase("Case123Tests", ExpectedResult = "Case 123 Tests")]
+    public string JoinCamel(string value, string separator = " ")
     {
-        return value.JoinCamelCase(separator);
+        return value.JoinCamel(separator);
     }
 }
